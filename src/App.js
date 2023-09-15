c1af4072fe252fae586cacfdd31d45bb4b0f6166
Nazemrap <corentin.damas@gmail.com>
2023-09-15 18:40:29 +0200

    Gallery main function, change img on clic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import { useState } from "react";
 
 export default function App() {
+  const [selectImg, onSelectImg] = useState(1);
+
   return (
     <>
-      <Gallery/>
-      <MainImage />
+      <Gallery selectImg={selectImg} />
+      <Thumbnails selectImg={selectImg} onSelectImg={onSelectImg} />
     </>
   );
 }
 
-function Gallery() {
-  return <h3 style={{ color: "red" }}>Hello from the React app</h3>;
-}
-
-function MainImage() {
+function Gallery({ selectImg }) {
   return (
     <div className="gallery-main-img">
-      <img src="./images/image-product-1.jpg" alt="gallery 1"/>
+      <img
+        src={`./images/image-product-${selectImg}.jpg`}
+        alt={`gallery ${selectImg}`}
+      />
     </div>
-  )
+  );
+}
+
+function Thumbnails({ onSelectImg, selectImg }) {
+  return (
+    <>
+      <div className="gallery-miniatures">
+        <Image number={1} selectImg={selectImg} onSelectImg={onSelectImg} />
+        <Image number={2} selectImg={selectImg} onSelectImg={onSelectImg} />
+        <Image number={3} selectImg={selectImg} onSelectImg={onSelectImg} />
+        <Image number={4} selectImg={selectImg} onSelectImg={onSelectImg} />
+      </div>
+    </>
+  );
+}
+
+function Image({ number, onSelectImg, selectImg }) {
+  return (
+    <img
+      className={` ${
+        selectImg === number
+          ? "gallery-thumbnail-selected"
+          : "gallery-thumbnail"
+      }`}
+      onClick={() => onSelectImg(number)}
+      src={`./images/image-product-${number}-thumbnail.jpg`}
+      alt={`miniarture ${number}`}
+    />
+  );
 }
